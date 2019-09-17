6ea60acb83509255216f4a65be4046cbc30a88f8
nehak2019 <kaurneha550@gmail.com>
2019-09-17 10:58:58 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>leaves animation</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<section>
+		<center><h1>Welcome</h1></center>
+		<div class="set">
+			<div><img src="l1.png" height="150px"  width="150px"></div>
+			<div><img src="l2.png" height="150px"  width="150px"></div>
+			<div><img src="l3.png" height="150px"  width="150px"></div>
+			<div><img src="l4.png"  height="150px"  width="150px"></div>
+			<div><img src="l1.png" height="150px"  width="150px"></div>
+			<div><img src="l2.png" height="150px"  width="150px"></div>
+			<div><img src="l3.png" height="150px"  width="150px"></div>
+			<div><img src="l4.png" height="150px"  width="150px"></div>
+			
+		</div>
+		<div class="set set2">
+			<div><img src="l1.png" height="150px"  width="150px"></div>
+			<div><img src="l2.png" height="150px"  width="150px"></div>
+			<div><img src="l3.png" height="150px"  width="150px"></div>
+			<div><img src="l4.png"  height="150px"  width="150px"></div>
+			<div><img src="l1.png" height="150px"  width="150px"></div>
+			<div><img src="l2.png" height="150px"  width="150px"></div>
+			<div><img src="l3.png" height="150px"  width="150px"></div>
+			<div><img src="l4.png" height="150px"  width="150px"></div>
+			
+		</div>
+		<div class="set set3">
+			<div><img src="l1.png" height="150px"  width="150px"></div>
+			<div><img src="l2.png" height="150px"  width="150px"></div>
+			<div><img src="l3.png" height="150px"  width="150px"></div>
+			<div><img src="l4.png"  height="150px"  width="150px"></div>
+			<div><img src="l1.png" height="150px"  width="150px"></div>
+			<div><img src="l2.png" height="150px"  width="150px"></div>
+			<div><img src="l3.png" height="150px"  width="150px"></div>
+			<div><img src="l4.png" height="150px"  width="150px"></div>
+			
+		</div>
+	</section>
+</body>
+
+</html>
